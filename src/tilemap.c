fda6e2378272d88a90ac3d029b16997aa78d7c47
lonskyne <lonskyne@gmail.com>
2024-11-20 20:50:49 +0100

    Begin random generation, in progress.

--- src/tilemap.c
+++ src/tilemap.c
@@ -1,16 +1,61 @@
 #include "tilemap.h"
 
+void generate_map(int map[WORLD_SIZE][WORLD_SIZE]) {
+  int helper[WORLD_SIZE][WORLD_SIZE];
+  int iterations = 5;
+
+  // Generate random noise with 45% of walls
+  for (int i = 0; i < WORLD_SIZE; i++) {
+    for (int j = 0; j < WORLD_SIZE; j++) {
+      if (randf() < 0.45f)
+        helper[i][j] = 1;
+      else
+        helper[i][j] = 0;
+    }
+  }
+
+  int **to = map, **from = helper;
+
+  for(int it = 0; it < iterations; it++) {
+
+    for(int i = 0; i < WORLD_SIZE; i++) {
+      for(int j = 0; j < WORLD_SIZE; j++) {
+        int neighbours = 0;
+
+        for(int k = -1; k < 2; k++) {
+          for(int l = -1; l < 2; l++) {
+            if(i + k >= 0 && i + k < WORLD_SIZE && j + l >= 0 && j + l < WORLD_SIZE) {
+              if(from[i + k][j + l] == 1)
+                neighbours++;
+            }
+          }
+        }
+
+        if(neighbours >= 5)
+          to[i][j] = 1;
+      }
+    }
+
+    int **temp = to;
+    to = from;
+    from = temp;
+  }
+}
+
 void init_tilemap(void) {
   int x, y;
+  int cave_map[WORLD_SIZE][WORLD_SIZE];
 
   for (y = 0; y < WORLD_SIZE; y++) {
     for (x = 0; x < WORLD_SIZE; x++) {
+      generate_map(cave_map);
       tile_world[y][x] = 3 + rand() % 2;
     }
   }
 }
 
-void render_tilemap(rafgl_raster_t *raster, int camx, int camy, int raster_width, int raster_height) {
+void render_tilemap(rafgl_raster_t *raster, int camx, int camy,
+                    int raster_width, int raster_height) {
   int x, y;
   int x0 = camx / TILE_SIZE;
 
@@ -45,5 +90,4 @@ void render_tilemap(rafgl_raster_t *raster, int camx, int camy, int raster_width
                                y * TILE_SIZE - camy);
     }
   }
-
 }
